ca4946e7263146316912b98a543c844709fd3d79
Andrew-Biggins1 <116644810+Andrew-Biggins1@users.noreply.github.com>
2024-05-11 18:11:08 +0800

    css for login/signup. update to deliverable
    changed the deliverable name. updating css and html. css for signup and login pages. added html to signup.

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template
 
-# Create the Flask application instance
+
 flaskApp = Flask(__name__)
 
-# Define the homepage route and view
+
 @flaskApp.route('/')
 def homepage():
     return render_template('home.html')
