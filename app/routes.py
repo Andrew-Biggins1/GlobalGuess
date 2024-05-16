d561fc623f8f8a4ce250870bb0799928c8271cdf
Andrew-Biggins1 <116644810+Andrew-Biggins1@users.noreply.github.com>
2024-05-16 11:51:04 +0800

    database
    working on routes and database for user registration

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import redirect, render_template, request, url_for
+from flask import redirect, render_template, request, url_for, flash, jsonify, session
 from app import flaskApp,db
 from app.models import User
 
@@ -30,8 +30,28 @@ def registerComplete():
         email = request.form['email']
         username = request.form['username']
         password = request.form['password']
-        new_user = User(email=email, username=username, password=password)
-        db.session.add(new_user)
-        db.session.commit()
-        return redirect(url_for('login'))
-    return render_template('register.html')
+
+        if not email or not username or not password:
+            flash('Please enter all required fields.', 'error')
+        elif len(username) > 15 or len(username) < 4:
+            flash('Username must be between 4 and 15 characters long.', 'error')
+        elif len(password) > 25 or len(password) < 6:
+            flash('Password must be between 6 and 25 characters long.', 'error')
+        elif len(email) >120:
+            flash('Please enter valid email.', 'error')
+        else:
+            CurUsername = username
+            session['js_username'] = CurUsername
+            
+            new_user = User(email=email, username=username, password=password)
+            db.session.add(new_user)
+            db.session.commit()
+            flash('Registration successful!', 'success')
+            return redirect(url_for('play'))
+    
+    return render_template('signup.html')
+
+@flaskApp.route('/get_username', methods=['GET'])
+def get_username():
+    js_username = session.get('js_username')
+    return jsonify({'currentUsername': js_username})
