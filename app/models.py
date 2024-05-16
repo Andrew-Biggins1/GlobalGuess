5e08a33f106e36929a34685e3d0fec4f966b3fe7
Andrew-Biggins1 <116644810+Andrew-Biggins1@users.noreply.github.com>
2024-05-16 17:36:03 +0800

    guess model
    added gues class

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from app import db
 
 
+#model for user
 class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     username = db.Column(db.String(15), unique=True, nullable=False, primary_key=True)
@@ -8,3 +9,10 @@ class User(db.Model):
 
     def __repr__(self):
         return f'<User {self.username}>'
+    
+#Model for guess made (Used to display in profile)
+class Guess(db.Model):
+    Guess = db.Column(db.Boolean, nullable = False)
+    correctGuess = db.Column(db.Boolean, nullable = False)
+    isCorrect = db.Column(db.Boolean, nullable = False)
+    
