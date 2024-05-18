8666d7e2323103b009a625f999817e3153b2ea74
Andrew-Biggins1 <116644810+Andrew-Biggins1@users.noreply.github.com>
2024-05-18 18:06:14 +0800

    accounts
    working on accounts

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 #model for user
@@ -20,4 +21,11 @@ class Guess(db.Model):
 
     def __repr__(self):
         return f'<Was your guess correct? {self.Guess == self.correctGuess}>'
+    
+    
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
     
--- app/routes.py
+++ app/routes.py
@@ -24,37 +24,36 @@ def profile():
 def play():
     return render_template("play.html")
 
-@flaskApp.route("/registered")
 
-
-@flaskApp.route('/register', methods=['GET', 'POST'])
+@flaskApp.route('/register', methods=['post'])
 def registerComplete():
-    if request.method == 'POST':
-        email = request.form['email']
-        username = request.form['username']
-        password = request.form['password']
+    email = request.form['email']
+    username = request.form['username']
+    password = request.form['password']
+    #print(f'Email: {email}')
+    #print(f'Username: {username}')
+    #print(f'Password: {password}')
 
-        if not email or not username or not password:
-            flash('Please enter all required fields.', 'error')
-        elif len(username) > 15 or len(username) < 4:
-            flash('Username must be between 4 and 15 characters long.', 'error')
-        elif len(password) > 25 or len(password) < 6:
-            flash('Password must be between 6 and 25 characters long.', 'error')
-        elif len(email) >120:
-            flash('Please enter valid email.', 'error')
-        else:
-            CurUsername = username
-            session['js_username'] = CurUsername
-            
-            new_user = User(email=email, username=username, password=password)
-            db.session.add(new_user)
-            db.session.commit()
-            flash('Registration successful!', 'success')
-            return redirect(url_for('play'))
     
-    return render_template('signup.html')
+    if not email or not username or not password:
+        flash('Please enter all required fields.', 'error')
+    elif len(username) > 15 or len(username) < 4:
+        flash('Username must be between 4 and 15 characters long.', 'error')
+    elif len(password) > 25 or len(password) < 6:
+        flash('Password must be between 6 and 25 characters long.', 'error')
+    elif len(email) >120:
+        flash('Please enter valid email.', 'error')
+    else:
+        username
+        session['js_username'] = username
+            
+        new_user = User(email=email, username=username, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+        flash('Registration successful!', 'success')
+        return redirect(url_for('play'))
+        
 
-@flaskApp.route('/get_username', methods=['GET'])
-def get_username():
-    js_username = session.get('js_username')
-    return jsonify({'currentUsername': js_username})
+@flaskApp.route('/login')
+def login():
+    
