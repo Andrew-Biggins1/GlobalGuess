161ac9a64c60d75c400246abfbff125abae49783
leo-mcgill <leomcgill1@icloud.com>
2024-05-18 19:38:16 +0800

    polished up css for the play page. Added localStorage functions to store username and points across pages. The points will be reset whenever the home page is loaded.

--- app/static/main.js
+++ app/static/main.js
@@ -1,7 +1,3 @@
-var currentUsername;
-var currentScore;
-
-
 
 //Ajax request to grab username to be displayed
 $(document).ready(function() {
@@ -113,7 +109,7 @@ function showFileInput() {
 function loadPlay(){
     var userDisplay = document.getElementById("userNameDisplay");
     updateImageView();
-    userDisplay.textContent = localStorage.getItem('username');
+    //userDisplay.textContent = localStorage.getItem('username');
 
     userNumber = getRandomUser();
     populateDivs(userNumber);
